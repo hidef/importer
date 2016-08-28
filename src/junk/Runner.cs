90dfe54c0d9ebcc434d0d5ea32a30ddc117bb870
Robert Stiff <uatecuk@gmail.com>
2016-08-28 19:01:49 +0100

    Message counter so we can see progress, and error handling so we know when something's gone wrong but don't die.

--- src/junk/Runner.cs
+++ src/junk/Runner.cs
@@ -7,9 +7,18 @@ namespace import
         public static void Run<T>(this Destination<T> destination)
         {
             Console.WriteLine("Running...");
+            int messageCount = 0;
             while ( true ) 
             {
-                destination.Get();
+                try
+                {
+                    destination.Get();
+                    Console.WriteLine($"Message Count: {messageCount++}");
+                }
+                catch ( AggregateException ex ) 
+                {
+                    Console.WriteLine($"ERROR: {ex.InnerException.GetType().Name} {ex.InnerException.Message}");
+                }
             }
             Console.WriteLine("Done.");
         }
